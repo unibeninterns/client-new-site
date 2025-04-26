155c0cf582beb10358e719cc7efd6faeb1790484
udezueoluomachi <basilchimaobi2@gmail.com>
2025-04-26 16:16:06 +0100

    added new project path

---
+++ src/app/masters-funding/laylout.tsx
@@ -0,0 +1,4 @@
+export const metadata = {
+    title: "Master’s Proposal Submission - DRID, University of Benin",
+    description: "Submit your concept note for the Pilot Seed Funding for Research-Driven Innovation by the Directorate of Research, Innovation and Development (DRID), University of Benin. Open to Master’s students only."
+}
